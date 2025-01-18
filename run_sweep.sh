6ebab52b3489e05da8ea9ada405f40bfbb9f9cc1
the-puzzler <172930174+the-puzzler@users.noreply.github.com>
2025-01-18 12:22:43 +0100

    runsweep bug fixed

--- run_sweep.sh
+++ run_sweep.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Number of agents to run
-NUM_AGENTS=4
+NUM_AGENTS=1
 
 # Name of your Python script
 PYTHON_SCRIPT="train_script_wbsweep_save_models.py"
 
 # Loop to start each agent
-for ((i=1; i<=NUM_AGENTS; i++))
+for i in $(seq 1 $NUM_AGENTS)
 do
     echo "Starting agent $i"
     python $PYTHON_SCRIPT &
